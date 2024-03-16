bec30c4af18e5e48157ffabce5d9f2af01b7fdd3
JorgeAugustoWilchen <120045345+JorgeAugustoWilchen@users.noreply.github.com>
2024-03-15 22:08:33 -0300

    adicionado paginacao

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,41 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
 
+require 'faker'
+
+# Limpa todos os registros existentes
+Debt.destroy_all
+Person.destroy_all
 User.destroy_all
 
-User.create email: 'admin@admin.com', password: '111111'
-
+# Criação do usuário admin
+admin_user = User.create(email: 'admin@admin.com', password: '111111')
 puts "Usuário criado:"
-puts "login admin@admin.com"
-puts "111111"
+puts "Login: #{admin_user.email}"
+puts "Senha: #{admin_user.password}"
+
+# Gera 50 usuários com dados falsos
+3000.times do
+  user = User.create(email: Faker::Internet.email, password: Faker::Internet.password)
+
+  # Para cada usuário, cria 2 pessoas com dados falsos
+  2.times do
+    person = Person.create(
+      name: Faker::Name.name,
+      phone_number: Faker::PhoneNumber.phone_number,
+      national_id: CPF.generate,
+      active: [true, false].sample,
+      user: user
+    )
+
+    # Para cada pessoa, cria 5 dívidas
+    5.times do
+      Debt.create(
+        amount: Faker::Number.decimal(l_digits: 2),
+        observation: Faker::Lorem.sentence,
+        person: person
+      )
+    end
+  end
+end
+
+puts "50 usuários, 100 pessoas e 500 dívidas criados com dados falsos."
