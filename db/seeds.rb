c8ccccb7aec52a6cdd9caf8d8aa53b2bad54001a
Tiago Geraldi <tiagogeraldi@msn.com>
2024-03-18 22:20:55 -0300

    Add faker, performance and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,36 @@
-# db/seeds.rb
-
-require 'faker'
-
-# Limpa todos os registros existentes
+puts "Destroying existing records..."
+User.destroy_all
 Debt.destroy_all
 Person.destroy_all
-User.destroy_all
 
 # Criação do usuário admin
 admin_user = User.create(email: 'admin@admin.com', password: '111111')
 puts "Usuário criado:"
-puts "Login: #{admin_user.email}"
-puts "Senha: #{admin_user.password}"
+puts "login admin@admin.com"
+puts "111111"
 
-# Gera 50 usuários com dados falsos
-3000.times do
-  user = User.create(email: Faker::Internet.email, password: Faker::Internet.password)
-
-  # Para cada usuário, cria 2 pessoas com dados falsos
-  2.times do
-    person = Person.create(
-      name: Faker::Name.name,
-      phone_number: Faker::PhoneNumber.phone_number,
-      national_id: CPF.generate,
-      active: [true, false].sample,
-      user: user
-    )
-
-    # Para cada pessoa, cria 5 dívidas
-    5.times do
-      Debt.create(
-        amount: Faker::Number.decimal(l_digits: 2),
-        observation: Faker::Lorem.sentence,
-        person: person
-      )
-    end
-  end
+1000.times do |counter|
+  puts "Creating user #{counter}"
+  User.create email: Faker::Internet.email, password: '111111'
 end
 
-puts "50 usuários, 100 pessoas e 500 dívidas criados com dados falsos."
+3000.times do |counter|
+  puts "Inserting Person #{counter}"
+
+  attrs = {
+    name: Faker::Name.name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    national_id: CPF.generate,
+    active: [true, false].sample,
+    user: User.order('random()').first
+  }
+  person = Person.create(attrs)
+
+  5.times do |debt_counter|
+    puts "Inserting Debt #{debt_counter}"
+    person.debts.create(
+      amount: Faker::Number.between(from: 1, to: 200),
+      observation: Faker::Lorem.paragraph
+    )
+  end
+end
